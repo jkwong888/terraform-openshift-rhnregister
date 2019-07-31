ae7255d0c6ba3c0b9975911b73c6d7babe5a40ac
Noel Colon <ncolon@us.ibm.com>
2019-07-31 02:04:24 -0400

    fix/sync

--- rhnregister.tf
+++ rhnregister.tf
@@ -1,12 +1,16 @@
-resource "null_resource" "setup_storage" {
-    count = "${var.storage["nodes"]}"
+locals {
+    all_hosts_ip_address = "${concat(list(var.bastion_ip_address), var.master_ip_address, var.infra_ip_address, var.app_ip_address, var.storage_ip_address, var.haproxy_ip_address)}"
+}
+
+resource "null_resource" "rhnregister" {
+    count = "${var.bastion["nodes"] + var.master["nodes"] + var.infra["nodes"] + var.worker["nodes"] + var.storage["nodes"] + var.haproxy["nodes"]}"
     connection {
         type     = "ssh"
         user     = "${var.ssh_username}"
-        host = "${element(var.storage_ip_address, count.index)}"
-        private_key = "${file(var.storage_private_ssh_key)}"
+        host = "${element(local.all_hosts_ip_address, count.index)}"
+        private_key = "${file(var.private_ssh_key)}"
         bastion_host = "${var.bastion_ip_address}"
-        bastion_host_key = "${file(var.bastion_private_ssh_key)}"
+        bastion_host_key = "${file(var.private_ssh_key)}"
     }
 
     provisioner "file" {
@@ -14,6 +18,7 @@ resource "null_resource" "setup_storage" {
         source      = "${path.module}/scripts"
         destination = "/tmp"
     }
+
 
     provisioner "remote-exec" {
         when = "create"
--- variables.tf
+++ variables.tf
@@ -1,61 +1,14 @@
-
-variable "app_ip_address" {
-  type = "list"
-}
-
-
-variable "app_private_ssh_key" {
-
-}
-
-variable "bastion_ip_address" {
-
-}
+variable "bastion_ip_address" {}
+variable "haproxy_ip_address" { type = "list"}
+variable "app_ip_address"     { type = "list" }
+variable "infra_ip_address"   { type = "list" }
+variable "master_ip_address"  { type = "list" }
+variable "storage_ip_address" { type = "list" }
 
 
-variable "bastion_private_ssh_key" {
-
-}
-
-
-variable "infra_ip_address" {
-  type = "list"
-}
-
-
-variable "infra_private_ssh_key" {
-
-}
-
-variable "master_ip_address" {
-  type = "list"
-}
-
-
-variable "master_private_ssh_key" {
-
-}
-
-variable "storage_ip_address" {
-  type = "list"
-}
-
-
-variable "storage_private_ssh_key" {
-
-}
-
-variable "rhn_username"{
-
-}
-
-variable "rhn_password"{
-
-}
-
-variable "rhn_poolid"{
-
-}
+variable "rhn_username" {}
+variable "rhn_password" {}
+variable "rhn_poolid" {}
 
 variable "ssh_username" {
     default = "root"
@@ -67,3 +20,4 @@ variable "infra"   {type = "map"}
 variable "worker"  {type = "map"}
 variable "storage" {type = "map"}
 variable "haproxy" {type = "map"}
+variable "private_ssh_key" {}
