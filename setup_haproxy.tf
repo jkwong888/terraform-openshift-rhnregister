c3065f5778538d937cb8db7d9d0eef6dee80d0c2
Noel Colon <ncolon@us.ibm.com>
2019-07-30 21:25:48 -0400

    add haproxy loadbalancers

---
+++ setup_haproxy.tf
@@ -0,0 +1,32 @@
+resource "null_resource" "setup_haproxy" {
+    count = "${var.haproxy["nodes"]}"
+    connection {
+        type     = "ssh"
+        user     = "${var.ssh_username}"
+        host = "${element(var.haproxy_ip_address, count.index)}"
+        private_key = "${file(var.haproxy_private_ssh_key)}"
+        bastion_host = "${var.bastion_ip_address}"
+        bastion_host_key = "${file(var.bastion_private_ssh_key)}"
+    }
+
+    provisioner "file" {
+        when = "create"
+        source      = "${path.module}/scripts"
+        destination = "/tmp"
+    }
+
+    provisioner "remote-exec" {
+        when = "create"
+        inline = [
+            "sudo chmod +x /tmp/scripts/*",
+            "sudo /tmp/scripts/rhn_register.sh ${var.rhn_username} ${var.rhn_password} ${var.rhn_poolid}",
+        ]
+    }
+
+    provisioner "remote-exec" {
+        when = "destroy"
+        inline = [
+            "sudo subscription-manager unregister",
+        ]
+    }
+}
