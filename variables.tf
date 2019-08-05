359320dcaad97e35927a2844225be00f431ba8b8
Noel Colon <ncolon@us.ibm.com>
2019-08-05 09:21:00 -0400

    haproxy_ip_address optional

--- variables.tf
+++ variables.tf
@@ -1,9 +1,12 @@
 variable "bastion_ip_address" {}
-variable "haproxy_ip_address" { type = "list"}
 variable "app_ip_address"     { type = "list" }
 variable "infra_ip_address"   { type = "list" }
 variable "master_ip_address"  { type = "list" }
 variable "storage_ip_address" { type = "list" }
+variable "haproxy_ip_address" {
+    type = "list"
+    default = []
+}
 
 
 variable "rhn_username" {}
