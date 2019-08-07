b6d4c25c25f7a5ec67c05f586871ccd86919af96
Noel Colon <ncolon@us.ibm.com>
2019-08-07 11:14:39 -0400

    simplify node list

--- rhnregister.tf
+++ rhnregister.tf
@@ -1,13 +1,9 @@
-locals {
-    all_hosts_ip_address = "${concat(list(var.bastion_ip_address), var.master_ip_address, var.infra_ip_address, var.app_ip_address, var.storage_ip_address, var.haproxy_ip_address)}"
-}
-
 resource "null_resource" "rhnregister" {
-    count = "${var.bastion["nodes"] + var.master["nodes"] + var.infra["nodes"] + var.worker["nodes"] + var.storage["nodes"] + var.haproxy["nodes"]}"
+    count = "${var.all_count}"
     connection {
         type     = "ssh"
         user     = "${var.ssh_username}"
-        host = "${element(local.all_hosts_ip_address, count.index)}"
+        host = "${element(var.all_nodes, count.index)}"
         private_key = "${file(var.private_ssh_key)}"
         bastion_host = "${var.bastion_ip_address}"
         bastion_host_key = "${file(var.private_ssh_key)}"
--- variables.tf
+++ variables.tf
@@ -1,14 +1,6 @@
 variable "bastion_ip_address" {}
-variable "app_ip_address"     { type = "list" }
-variable "infra_ip_address"   { type = "list" }
-variable "master_ip_address"  { type = "list" }
-variable "storage_ip_address" { type = "list" }
-variable "haproxy_ip_address" {
-    type = "list"
-    default = []
-}
-
-
+variable "all_nodes" { type = "list" }
+variable "all_count" {}
 variable "rhn_username" {}
 variable "rhn_password" {}
 variable "rhn_poolid" {}
@@ -16,11 +8,4 @@ variable "rhn_poolid" {}
 variable "ssh_username" {
     default = "root"
 }
-
-variable "bastion" {type = "map"}
-variable "master"  {type = "map"}
-variable "infra"   {type = "map"}
-variable "worker"  {type = "map"}
-variable "storage" {type = "map"}
-variable "haproxy" {type = "map"}
 variable "private_ssh_key" {}
