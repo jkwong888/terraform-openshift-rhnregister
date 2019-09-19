371d4f7087e2b350fea4b5d2b67ccb1e3324ba85
jkwong888 <jkwong@ca.ibm.com>
2019-09-19 17:50:51 -0400

    use ansible playbook module to do all script execution

--- register.tf
+++ register.tf
@@ -1,55 +1,39 @@
-data "template_file" "rhn_register_sh" {
-  vars = {
-    verbosity = ""
+module "runplaybook" {
+  source = "github.com/ibm-cloud-architecture/terraform-ansible-runplaybooks.git"
 
-    rhel_user_name = "${var.rhn_username}"
-    rhel_password  = "${var.rhn_password}"
-    subscription_pool = "${var.rhn_poolid}"
+  ansible_playbook_dir = "${path.module}/playbooks"
+  ansible_playbooks = [
+    "playbooks/configure_rhn.yaml"
+  ]
+
+  ansible_playbooks_destroy = [
+    "playbooks/unregister.yaml"
+  ]
+
+  ssh_user           = "${var.ssh_user}"
+  ssh_password       = "${var.ssh_password}"
+  ssh_private_key    = "${var.ssh_private_key}"
+
+  bastion_ip_address       = "${var.bastion_ip_address}"
+  bastion_ssh_user         = "${var.bastion_ssh_user}"
+  bastion_ssh_password     = "${var.bastion_ssh_password}"
+  bastion_ssh_private_key  = "${var.bastion_ssh_private_key}"
+
+  node_ips = "${var.all_nodes}"
+  node_hostnames = "${var.all_nodes}"
+
+  dependson = "${var.dependson}"
+
+  triggerson = {
+    node_ips = "${join(",", var.all_nodes)}"
   }
 
-  template = "${file("${path.module}/templates/rhn_register.sh.tpl")}"
+  ansible_vars       = {
+    "rhn_username" = "${var.rhn_username}"
+    "rhn_password" = "${var.rhn_password}"
+    "rhn_poolid"  = "${var.rhn_poolid}"
+  }
 
+  # ansible_verbosity = "-vvv"
 }
 
-resource "null_resource" "rhn_register" {
-    count = "${var.all_count}"
-
-    triggers = {
-      node_list = "${join(",", var.all_nodes)}"
-    }
-
-    connection {
-        type     = "ssh"
-        host        = "${element(var.all_nodes, count.index)}"
-        user        = "${var.ssh_user}"
-        password    = "${var.ssh_password}"
-        private_key = "${var.ssh_private_key}"
-
-        bastion_host        = "${var.bastion_ip_address}"
-        bastion_user        = "${var.bastion_ssh_user}"
-        bastion_password    = "${var.bastion_ssh_password}"
-        bastion_private_key = "${var.bastion_ssh_private_key}"
-    }
-
-    provisioner "file" {
-        when = "create"
-        content      = "${data.template_file.rhn_register_sh.rendered}"
-        destination = "/tmp/rhn_register.sh"
-    }
-
-    provisioner "remote-exec" {
-        when = "create"
-        inline = [
-            "chmod +x /tmp/rhn_register.sh",
-            "sudo /tmp/rhn_register.sh",
-            "rm -f /tmp/rhn_register.sh"
-        ]
-    }
-
-    provisioner "remote-exec" {
-        when = "destroy"
-        inline = [
-            "sudo subscription-manager unregister",
-        ]
-    }
-}
