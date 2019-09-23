07185cf61e59306a8b36f09afd015d276f0b82e9
Jeffrey Kwong <jkwong888@users.noreply.github.com>
2019-09-23 14:29:45 -0400

    allow overwrite parts of rhsm.conf to support ibm cloud (#5)
    * simplify
    
    * add bastion ssh password
    
    * add logs
    
    * retrigger registration if nodelist changes
    
    * latest update
    
    * update link in readme
    
    * add example
    
    * use ansible playbook module to do all script execution
    
    * allow overwrite parts of rhsm.conf to support register on ibmcloud

--- variables.tf
+++ variables.tf
@@ -34,6 +34,27 @@ variable "rhn_password"{
 variable "rhn_poolid"{
 }
 
+variable "rhn_server_hostname" {
+  default = "subscription.rhsm.redhat.com"
+}
+
+variable "rhn_server_prefix" {
+  default = "/subscription"
+}
+
+variable "rhn_server_port" {
+  default = "443"
+}
+
+variable "rhn_rhsm_baseurl" {
+  default = "https://cdn.redhat.com"
+}
+
+variable "rhn_rhsm_repo_ca_cert" {
+  default = "/etc/rhsm/ca/redhat-uep.pem"
+}
+
+
 variable "all_nodes" {
   type = "list"
   default = []
